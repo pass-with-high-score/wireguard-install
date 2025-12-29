bd6946ed1d7b347b8fed3e6925e694c99cb7fdf5
Phuonghth <hhpcv7676@gmail.com>
2025-12-29 15:19:16 +0700

    fix: streamline IPv4 client validation in wireguard installation script

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -377,17 +377,13 @@ function newClient() {
 	fi
 
 	BASE_IP=$(echo "$SERVER_WG_IPV4" | awk -F '.' '{ print $1"."$2"."$3 }')
-	until [[ ${IPV4_EXISTS} == '0' ]]; do
-		read -rp "Client WireGuard IPv4: ${BASE_IP}." -e -i "${DOT_IP}" DOT_IP
-		CLIENT_WG_IPV4="${BASE_IP}.${DOT_IP}"
-		IPV4_EXISTS=$(grep -c "$CLIENT_WG_IPV4/32" "/etc/wireguard/${SERVER_WG_NIC}.conf")
+  CLIENT_WG_IPV4="${BASE_IP}.${DOT_IP}"
 
-		if [[ ${IPV4_EXISTS} != 0 ]]; then
-			echo ""
-			echo -e "${ORANGE}A client with the specified IPv4 was already created, please choose another IPv4.${NC}"
-			echo ""
-		fi
-	done
+  IPV4_EXISTS=$(grep -c "$CLIENT_WG_IPV4/32" "/etc/wireguard/${SERVER_WG_NIC}.conf")
+  if [[ ${IPV4_EXISTS} != 0 ]]; then
+  	echo "IPv4 already exists: ${CLIENT_WG_IPV4}"
+  	exit 1
+  fi
 
 	BASE_IP=$(echo "$SERVER_WG_IPV6" | awk -F '::' '{ print $1 }')
 	until [[ ${IPV6_EXISTS} == '0' ]]; do
